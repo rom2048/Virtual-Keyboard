d595ba6fda89ae7208f033bc57b074d430e65e57
Roman <mamontov.r1995@gmail.com>
2022-05-06 21:37:41 +0300

    feat: update app with switch language functional keys and print to text area

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
+import  App  from "./app/app";
+import { get } from './app/storage/storage';
+import './style.css';
 
+const language = get('keyboardLang', '"en"');
+
+const rowsOrder = [
+    ['Backquote', 'Digit1', 'Digit2', 'Digit3', 'Digit4', 'Digit5', 'Digit6', 'Digit7', 'Digit8', 'Digit9', 'Digit0', 'Minus', 'Equal', 'Backspace'],
+    ['Tab', 'KeyQ', 'KeyW', 'KeyE', 'KeyR', 'KeyT', 'KeyY', 'KeyU', 'KeyI', 'KeyO', 'KeyP', 'BracketLeft', 'BracketRight', 'Backslash'],
+    ['CapsLock', 'KeyA', 'KeyS', 'KeyD', 'KeyF', 'KeyG', 'KeyH', 'KeyJ', 'KeyK', 'KeyL', 'Semicolon', 'Quote', 'Enter'],
+    ['ShiftLeft', 'KeyZ', 'KeyX', 'KeyC', 'KeyV', 'KeyB', 'KeyN', 'KeyM', 'Comma', 'Period', 'Slash', 'ShiftRight', 'ArrowUp', 'Delete'],
+    ['ControlLeft', 'MetaLeft', 'AltLeft', 'Space', 'AltRight', 'ContextMenu', 'ControlRight', 'ArrowLeft', 'ArrowDown', 'ArrowRight']
+]
+
+const app = new App(document.body, language).generateKeyboard(rowsOrder);
---
+++ src/utils/button.js
@@ -0,0 +1,23 @@
+import Control from "./control";
+import button from '../app/styles/button.css';
+
+class Button {
+    constructor(parentNode, { small, shift, code }){
+        this.code = code;
+        this.small = small;
+        this.shift = shift;
+        this.isFnKey = Boolean(small.match(/Ctrl|arr|Alt|Shift|Tab|Back|Del|Enter|Caps/));
+        this.button = new Control(parentNode, 'div', button['button__wrapper'], '', ['code', this.code],this.isFnKey ? ["fn", "true"] : ["fn", "false"]);
+        if (this.isFnKey) {
+            this.button.node.classList.add(button["key-fn"]);
+        }
+        this.char = new Control(this.button.node, 'div', button['key'], this.small);
+        if (shift && shift.match(/[^a-zA-Zа-яА-ЯёЁ0-9]/)) {
+            this.sub = new Control(this.button.node, "div", button["sub"], this.shift);
+        } else {
+            this.sub = new Control(this.button.node, "div", button["sub"]);
+        }
+    }
+}
+
+export default Button;
