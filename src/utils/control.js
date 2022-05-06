9ac75600f8850816a9d2f14f315900e0d974c37a
Roman <mamontov.r1995@gmail.com>
2022-05-06 22:15:49 +0300

    refactor: update .js files with eslint

--- src/utils/control.js
+++ src/utils/control.js
@@ -1,36 +1,36 @@
 class Control {
-    constructor(parentNode = null, tagName = 'div', className = '', content = '', ...dataAttr) {
-        const element = document.createElement(tagName);
-        element.className = className;
-        if (content === 'object Object') {
-            element.append(content);
+  constructor(parentNode = null, tagName = 'div', className = '', content = '', ...dataAttr) {
+    const element = document.createElement(tagName);
+    element.className = className;
+    if (content === 'object Object') {
+      element.append(content);
+    } else {
+      element.innerHTML = content;
+    }
+    if (parentNode) {
+      if (parentNode === document.body) {
+        parentNode.prepend(element);
+      } else {
+        parentNode.append(element);
+      }
+    }
+    if (dataAttr.length) {
+      dataAttr.forEach(([attrName, attrValue]) => {
+        if (attrValue === '') element.setAttribute(attrName, '');
+        if (attrName.match(/value|id|placeholder|cols|rows|autocorrect|spellcheck/)) {
+          element.setAttribute(attrName, attrValue);
         } else {
-            element.innerHTML = content;
+          element.dataset[attrName] = attrValue;
         }
-        if (parentNode) {
-            if (parentNode === document.body) {
-                parentNode.prepend(element);
-            } else {
-                parentNode.append(element);
-            }
-        }
-        if (dataAttr.length) {
-            dataAttr.forEach(([attrName, attrValue]) => {
-                if (attrValue === '') element.setAttribute(attrName, '');
-                if (attrName.match(/value|id|placeholder|cols|rows|autocorrect|spellcheck/)) {
-                    element.setAttribute(attrName, attrValue);
-                } else {
-                    element.dataset[attrName] = attrValue;
-                }
-            });
-        }
-
-        this.node = element;
+      });
     }
 
-    destroy(){
-        this.node.remove();
-    }
+    this.node = element;
+  }
+
+  destroy() {
+    this.node.remove();
+  }
 }
 
 export default Control;
